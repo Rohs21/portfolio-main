a26d63fdeaafdafd5387589e386fef53a888d97d
Rohs21 <rohans212004@gmail.com>
2025-12-25 00:54:12 +0530

    project updated

--- portfolio/src/lib/constants.ts
+++ portfolio/src/lib/constants.ts
@@ -22,16 +22,27 @@ export const experienceData: I_Experience[] = [
   {
     company_link: "https://akashdayalgroups.com",
     company_logo: "/assets/akash-dayal.png",
-    company_name: "Akash Dayal Groups",
+    company_name: "ADG Pvt. Ltd.",
     additional_info: "",
     duration: "December 2024 - June 2025",
-    job_title: "Web Developer and Design Intern",
+    job_title: "Web Developer Intern",
     description:
-      "Designed, optimized, and tested 8+ responsive, reusable web pages, enhancing cross-device performance, accessibility, and UI/UX consistency to improve user engagement.",
+      "Designed, optimized, and tested 12+ responsive, reusable web pages, enhancing cross-device performance, accessibility, and UI/UX consistency to improve user engagement.",
   },
 ];
 
 export const projects = [
+  
+  {
+    title: "SyncSketch",
+    description: "Built a custom Excalidraw-like collaborative drawing app from scratch, implementing real-time collaboration with HTTP and WebSockets, and using Prisma ORM for efficient backend development.",
+    tech: ["Next.js", "Typescript", "TurboRepo", "Prisma", "NeonDB", ,"WebSockets","More.."],
+    status: "Live",
+    year: "2025",
+    link: "https://syncsketch-rkwk.onrender.com/",
+    code: "https://github.com/Rohs21/Draw-App",
+    imageSrc: "/projects/SyncSketch.png",
+  },
   {
     title: "Neuro-Learn Ai",
     description: "AI-powered NeuroLearn is a smart learning platform offering personalized content, real-time quizzes, automated summaries, and advanced interview prep with voice recording, video call integration, and performance analytics to boost learning and career readiness.",
